dfa6ba5b28705d15297278d1fdb81aaf385ffd8a
Crowdbotics <team@crowdbotics.com>
2020-07-13 19:17:19 +0000

    Update store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,4 +2,18 @@ import * as types from "./constants"
 
 const initialState = {}
 
-export default function apiReducer(state = initialState, action) {}
+export default function apiReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.GET_ALBUMS_LIST:
+    case types.GET_ALBUMS_LIST_SUCCEEDED:
+    case types.GET_ALBUMS_LIST_FAILED:
+    case types.GET_ALBUMS_ID_READ:
+    case types.GET_ALBUMS_ID_READ_SUCCEEDED:
+    case types.GET_ALBUMS_ID_READ_FAILED:
+    case types.GET_ALBUMS_ID_TRACKS_LIST:
+    case types.GET_ALBUMS_ID_TRACKS_LIST_SUCCEEDED:
+    case types.GET_ALBUMS_ID_TRACKS_LIST_FAILED:
+    default:
+      return state
+  }
+}
